9737860f72fa56a1cdd1fcb9dff9360077dd6aa3
Emel Topaloglu <emeltopaloglu@gmail.com>
2021-09-14 14:37:49 +0300

    Change legent color to yellow

--- Sources/SwiftUICharts/LineChart/IndicatorPoint.swift
+++ Sources/SwiftUICharts/LineChart/IndicatorPoint.swift
@@ -12,7 +12,7 @@ struct IndicatorPoint: View {
     var body: some View {
         ZStack{
             Circle()
-                .fill(Color.clear)
+                .fill(Color.LegendColor)
             Circle()
                 .stroke(Color.white, style: StrokeStyle(lineWidth: 4))
         }
